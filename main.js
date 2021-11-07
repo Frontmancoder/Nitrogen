d76d52cadd5dfe66d179b7e44db3460cc5e70233
Yo <93869651+Frontmancoder@users.noreply.github.com>
2021-11-07 19:38:18 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,60 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const randomstring = require("randomstring");
+
+const prefix = '!';
+
+client.on('message', msg => {
+
+    let args = msg.content.slice(prefix.length).trim().split(' ');
+
+    if (msg.content.startsWith(`${prefix}setup`)) {
+
+        if (!msg.guild.member(client.user).hasPermission(["MANAGE_CHANNELS", "ADMINISTRATOR"])) return;
+        msg.guild.channels.create(`mining`, 'text').catch(e => { });
+
+    }
+
+    if (msg.content.startsWith(`${prefix}gen`)) {
+
+        msg.delete()
+
+        msg.channel.send("https://discord.gift/" + randomstring.generate(16));
+
+    }
+
+
+    if (msg.content.startsWith(`${prefix}start`)) {
+        if (!msg.guild.member(client.user).hasPermission(["ADMINISTRATOR"])) return;
+
+        msg.delete()
+
+        var interval = setInterval(function () {
+
+            msg.channel.send("https://discord.gift/" + randomstring.generate(16));
+
+        }, 2000);
+
+    }
+
+});
+
+client.on('ready', async () => {
+    console.log('lets mine!')
+
+    let statuses = [
+        `skuva`,
+        `ok`,
+        `${client.users.cache.size} Miners👀`
+    ]
+
+    setInterval(function () {
+        let status = statuses[Math.floor(Math.random() * statuses.length)];
+        client.user.setActivity(status, {
+            type: "STREAMING",
+            url: "Discord Nitro"
+        })
+    }, 4000)
+})
+
+client.login('TOKEN')
